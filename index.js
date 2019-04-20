31bb26325cb537f1fb31b834548c837104c179eb
Patrick Candlin <pctennislessons@gmail.com>
2019-04-20 20:35:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,11 +2,18 @@
 function happyHolidays() {
   return "Happy holidays!"
 }
+
 var name = "Albert";
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
 var holiday = "Easter"
 function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
+}
+
+var days = "2"
+function holidayCountdown(days) {
+  return 
 }
