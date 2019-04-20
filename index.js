3fc6fce36bfac3c1db31955692ee4c19016568ca
Patrick Candlin <pctennislessons@gmail.com>
2019-04-20 20:39:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function happyHolidayTo(holiday, name) {
 }
 
 var days = "20"
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
